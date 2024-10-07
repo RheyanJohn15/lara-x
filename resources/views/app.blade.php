c4ee73e2306ed311d3303168b2b4bd8c8900ee10
RheyanJohn <rjvblanco.chmsu@gmail.com>
2024-10-07 17:50:49 +0800

    Initial Commit: Set Up Laravel/Vue/Tailwind

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		@vite(['resources/js/app.js'])
+        @vite('resources/css/app.css')
+	</head>
+	<body>
+		<div id="app">
+            <counter />
+		</div>
+	</body>
+</html>
